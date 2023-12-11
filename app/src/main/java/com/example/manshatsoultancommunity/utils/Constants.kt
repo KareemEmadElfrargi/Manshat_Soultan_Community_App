aa98ebf7c22649890765af1be59fb85205afbea0
mo samir <smyr03130@gmail.com>
2023-12-11 14:44:08 +0200

    add SharedPreferencesManager and extentions

--- app/src/main/java/com/example/manshatsoultancommunity/utils/Constants.kt
+++ app/src/main/java/com/example/manshatsoultancommunity/utils/Constants.kt
@@ -1,6 +1,8 @@
 package com.example.manshatsoultancommunity.utils
 
 object Constants {
+    const val SHARD_KEY = "manshat_soultan"
+
     const val CODE_AUTH_KEY = "codeAuthenticationKey"
     const val ADMIN_COLLECTION = "Admin"
 }
